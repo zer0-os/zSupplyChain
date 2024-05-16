747f78bfc781daea68add7dd5790933103895994
durienb <damien@wilderworld.com>
2024-05-15 23:35:49 -0600

    adding erc1155

--- Converters/contracts/Fabricator.sol
+++ Converters/contracts/Fabricator.sol
@@ -7,29 +7,31 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract Fabricator is Ownable{
+contract Fabricator is Ownable, ERC1155{
     struct Blueprint {
+        uint id;
         ERC20[] materials; //Which erc20 materials are needed for the build
         uint[] amountsRequired; //How many of each material is required per build
         uint fee;
     }
 
-    Blueprint[] blueprints;
+    Blueprint[] private blueprints;
 
-    constructor() Ownable(msg.sender){}
+    constructor(string memory baseURI) Ownable(msg.sender) ERC1155(baseURI) {}
 
-    function build(uint blueprintID) external payable {
-        Blueprint memory blueprint = getBlueprint(blueprintID);
-        require(msg.value == blueprint.fee, "Incorrect fee paid");
+    function fulfill(uint _blueprint, uint amount) external payable {
+        Blueprint memory blueprint = getBlueprint(_blueprint);
+        require(msg.value == blueprint.fee * amount, "Incorrect fee paid");
         for (uint i = 0; i < blueprint.materials.length; i++) {
             blueprint.materials[i].transferFrom(msg.sender, address(this), blueprint.amountsRequired[i]);
         }
+        _mint(msg.sender, blueprint.id, amount, );
     }
 
-    function addBlueprint(ERC20[] calldata materials, uint[] calldata amountsRequired, uint fee) external onlyOwner(){
-        blueprints.push(Blueprint(materials, amountsRequired, fee));
+    function addBlueprint(uint id, ERC20[] calldata materials, uint[] calldata amountsRequired, uint fee) external onlyOwner(){
+        blueprints.push(Blueprint(id, materials, amountsRequired, fee));
     }
 
     function collectFees() external onlyOwner() {

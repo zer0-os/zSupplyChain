a7e95a332fd6a71c0515e6d4545b41454ce7bbc0
durienb <damien@wilderworld.com>
2024-05-20 12:09:18 -0600

    Add expectation values

--- Converters/test/Refinery.ts
+++ Converters/test/Refinery.ts
@@ -48,32 +48,52 @@ describe("Converters", function () {
     const requiredAmounts = [60, 10]
     const fee = hre.ethers.parseEther("1");
     const blueprintID = "0";
-    //let blueprint: any;
 
     it("Adds new blueprint", async function () {
       const { refinery, materials } = await loadFixture(deploy);
   
       await refinery.addBlueprint(blueprintID, "uri.com", materials, requiredAmounts, fee);
+      const blueprint = await refinery.getBlueprint(blueprintID);
+      expect(blueprint.uri).to.equal("uri.com");
+      expect(blueprint.materials.length).to.equal(materials.length);
+      expect(blueprint.amountsRequired[0]).to.equal(requiredAmounts[0]);
+      expect(blueprint.amountsRequired[1]).to.equal(requiredAmounts[1]);
+      expect(blueprint.fee).to.equal(fee);
     });
+
     it("Gets blueprint", async function () {
       const { refinery, materials } = await loadFixture(deploy);
-  
+
       await refinery.addBlueprint(blueprintID, "uri.com", materials, requiredAmounts, fee);
+      const blueprint = await refinery.getBlueprint(blueprintID);
+      expect(blueprint.uri).to.equal("uri.com");
+      expect(blueprint.materials.length).to.equal(materials.length);
+      expect(blueprint.amountsRequired[0]).to.equal(requiredAmounts[0]);
+      expect(blueprint.amountsRequired[1]).to.equal(requiredAmounts[1]);
+      expect(blueprint.fee).to.equal(fee);
     });
+
     it("Builds blueprint", async function () {
       const { refinery, materials , user} = await loadFixture(deploy);
       
       await refinery.addBlueprint(blueprintID, "uri.com", materials, requiredAmounts, fee);
       
-      await refinery.connect(user).fulfill(blueprintID, 1, "0x00", {value: fee});
+      await expect(refinery.connect(user).fulfill(blueprintID, 1, "0x00", {value: fee}))
+        .to.changeTokenBalances(materials[0], [user, refinery], [-requiredAmounts[0], requiredAmounts[0]]);
+      expect(await refinery.balanceOf(user.address, blueprintID)).to.equal(1);
     });
+
     it("Collects fees", async function () {
       const { refinery, materials, owner, user } = await loadFixture(deploy);
       
       await refinery.addBlueprint(blueprintID, "uri.com", materials, requiredAmounts, fee);
       
       await refinery.connect(user).fulfill(blueprintID, 1, "0x00", {value: fee});
-      await refinery.connect(owner).collectFees();
+      const initialOwnerBalance = await hre.ethers.provider.getBalance(owner.address);
+      await expect(refinery.connect(owner).collectFees())
+        .to.changeEtherBalances([refinery, owner], [-fee, fee]);
+      const finalOwnerBalance = await hre.ethers.provider.getBalance(owner.address);
+      expect(finalOwnerBalance).to.be.above(initialOwnerBalance);
     });
   });
 });

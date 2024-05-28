6627fba55802a215d4d9a76433dd69c9d6d89835
durienb <damien@wilderworld.com>
2024-05-27 21:23:32 -0600

    add test steps

--- EVM/test/Resources.ts
+++ EVM/test/Resources.ts
@@ -22,6 +22,7 @@ import {
       const bondingTokenAddress = await bondingToken.getAddress();
   
       const mintAmt = hre.ethers.parseEther("1000");
+    
       await reserveToken.mint(userAddress, mintAmt);
       await reserveToken.mint(user1Address, mintAmt);
       await reserveToken.connect(user).approve(bondingTokenAddress, mintAmt);
@@ -34,45 +35,65 @@ import {
       it('Deploys bonding token', async function () {
         await loadFixture(deploy);
       });
-      it('User 1 deposits 1000 reserveToken, receives 1000 bondingToken', async function () {
+      it('User 1 spends 1000 reserveToken to buy 1000 bondingToken', async function () {
         const { bondingToken, user } = await loadFixture(deploy);
         const depositAmt = hre.ethers.parseEther("1000");
         await bondingToken.connect(user).deposit(depositAmt, user);
         const bal = await bondingToken.balanceOf(user);
         expect(bal).to.equal(depositAmt);
       });
-      it('User 2 deposits 1000 reserveToken, receives 1000 bondingToken', async function () {
-        const { bondingToken, user1 } = await loadFixture(deploy);
+      it('User 2 spends 1000 reserveToken to buy 1000 bondingToken', async function () {
+        const { bondingToken, user, user1 } = await loadFixture(deploy);
         const depositAmt = hre.ethers.parseEther("1000");
-        await bondingToken.connect(user1).deposit(depositAmt, user1);
-        const bal = await bondingToken.balanceOf(user1);
+        
+        await bondingToken.connect(user).deposit(depositAmt, user);
+        const bal = await bondingToken.balanceOf(user);
         expect(bal).to.equal(depositAmt);
+
+        await bondingToken.connect(user1).deposit(depositAmt, user1);
+        const bal1 = await bondingToken.balanceOf(user1);
+        expect(bal1).to.equal(depositAmt);
       });
-      it('User 1 redeems 1000 bondingToken, receives 1000 reserveToken', async function () {
-        const { bondingToken, user } = await loadFixture(deploy);
+      it('User 1 sells 1000 bondingToken to receive 1000 reserveToken', async function () {
+        const { bondingToken, user, user1 } = await loadFixture(deploy);
 
         const depositAmt = hre.ethers.parseEther("1000");
         await bondingToken.connect(user).deposit(depositAmt, user);
         const depBal = await bondingToken.balanceOf(user);
         expect(depBal).to.equal(depositAmt);
 
+        await bondingToken.connect(user1).deposit(depositAmt, user1);
+        const bal1 = await bondingToken.balanceOf(user1);
+        expect(bal1).to.equal(depositAmt);
+
         const redeemAmt = hre.ethers.parseEther("1000");
         await bondingToken.connect(user).redeem(redeemAmt, user, user);
         const bal = await bondingToken.balanceOf(user);
         expect(bal).to.equal(0);
       });
-      it('User 2 redeems 1000 bondingToken, receives 1000 reserveToken', async function () {
-        const { bondingToken, user1 } = await loadFixture(deploy);
+      it('User 2 sells 1000 bondingToken to receive 1000 reserveToken', async function () {
+        const { bondingToken, reserveToken, user, user1 } = await loadFixture(deploy);
 
         const depositAmt = hre.ethers.parseEther("1000");
-        await bondingToken.connect(user1).deposit(depositAmt, user1);
-        const depBal = await bondingToken.balanceOf(user1);
+        await bondingToken.connect(user).deposit(depositAmt, user);
+        const depBal = await bondingToken.balanceOf(user);
         expect(depBal).to.equal(depositAmt);
+        
+        await bondingToken.connect(user1).deposit(depositAmt, user1);
+        const depBal1 = await bondingToken.balanceOf(user1);
+        expect(depBal1).to.equal(depositAmt);
 
         const redeemAmt = hre.ethers.parseEther("1000");
-        await bondingToken.connect(user1).redeem(redeemAmt, user1, user1);
-        const bal = await bondingToken.balanceOf(user1);
+        
+        await bondingToken.connect(user).redeem(redeemAmt, user, user);
+        const bal = await bondingToken.balanceOf(user);
         expect(bal).to.equal(0);
+
+        await bondingToken.connect(user1).redeem(redeemAmt, user1, user1);
+        const bal1 = await bondingToken.balanceOf(user1);
+        const resBal = await reserveToken.balanceOf(user1);
+        expect(bal1).to.equal(0);
+        expect(resBal).to.equal(depositAmt);
       });
     });
   });
